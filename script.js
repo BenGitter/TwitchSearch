9e7f8b5703778e60c2246fafa8c5e74a07293e15
Benjamin <benjaminvanzwienen@live.nl>
2016-07-04 21:34:19 +0200

    Added functionality to sort buttons

--- script.js
+++ script.js
@@ -6,25 +6,36 @@ for(var i = 0; i < names.length; i++){
   makeRequest(names[i], i);
 }
 
-setTimeout(function(){
-    console.log(imgs);
-},2000);
+
 function makeRequest(name, i){
   $.getJSON('https://api.twitch.tv/kraken/streams/' + name + '?callback=?', function(data) {
     var status = "";
+    var imgURL = "";
+    var itemHTML = "";
     objects.push(data);
 
-    if(data.error != null){
-      status = "removed";
-    }else if(data.stream != null){
+    if("error" in data){
+      status = "error";
+    }else if(data.stream !== null){
       status = "online";
       imgs.push(data.stream.channel.logo);
+      imgURL = data.stream.channel.logo;
+      itemHTML = '<div id="' + name + '" class="col-xs-10 col-xs-offset-1 item"><div class="col-xs-3">';
+      itemHTML += '<img class="img-responsive" src="' + imgURL + '"  /></div><div class="col-xs-9">';
+      itemHTML += '<h3>' + name + '</h3></div></div>';
+
+      $("#onlineItems").append(itemHTML);
     }else{
       status = "offline";
-
+      itemHTML = '<div id="' + name + '" class="col-xs-10 col-xs-offset-1 item"><div class="col-xs-3">';
+      itemHTML += '<img class="img-responsive" src="' + imgURL + '"  /></div><div class="col-xs-9">';
+      itemHTML += '<h3>' + name + '</h3></div></div>';
+      $("#offlineItems").append(itemHTML);
+      getLogoOffline(name);
     }
-    var itemHTML = "<d";
-    //$("#status lu").append("<li id='item" + i + "'>" + name + ": " + status + "</li>");
+
+    console.log(status);
+
   });
 }
 
@@ -35,12 +46,14 @@ function makeRequest(name, i){
 //});
 
 function getLogoOffline(channel){
-  return $.getJSON('https://api.twitch.tv/kraken/channels/' + channel, function(data){
+  $.getJSON('https://api.twitch.tv/kraken/channels/' + channel, function(data){
+    var id = "#" + channel;
+    $(id).find(".img-responsive").attr('src', data.logo);
   });
 }
-
-var d = getLogoOffline("freecodecamp");
-console.log(d);
+//
+// var d = getLogoOffline("freecodecamp");
+// console.log(d);
 
 function resizing(){
   $("#iconBar input").width($("#iconBar").width()-$("#iconBar .btn-group").width()-30);
@@ -48,3 +61,39 @@ function resizing(){
 
 resizing();
 $(window).on("resize", resizing);
+
+// Event handlers sort buttons
+// Maybe use list functions?
+oldState = "all";
+$(".btn-group .btn").on('click', function(){
+    var newState = $(this).data("sort");
+    if(newState !== oldState){
+      if(oldState === "all"){
+        if(newState === "online"){
+          $("#offlineItems").slideUp(400);
+        }else{
+          $("#onlineItems").slideUp(400);
+        }
+      }
+
+      if(oldState === "online"){
+        if(newState === "all"){
+          $("#offlineItems").slideDown(400);
+        }else{
+          $("#onlineItems").slideUp(400);
+          $("#offlineItems").slideDown(400);
+        }
+      }
+
+      if(oldState === "offline"){
+        if(newState === "all"){
+          $("#onlineItems").slideDown(400);
+        }else{
+          $("#onlineItems").slideDown(400);
+          $("#offlineItems").slideUp(400);
+        }
+      }
+      oldState = newState;
+    }
+
+});
