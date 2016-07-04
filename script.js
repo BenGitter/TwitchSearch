415c343afc3d8623e36c8c679c60ef16e3097bd2
Benjamin van Zwienen <Benjamin van Zwienen>
2016-07-04 18:37:55 +0200

    Added Ajax requests

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 /* Check if channel in list is online/offline/removed */
 var names = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "brunofin", "noobs2ninjas"];
 var objects = [];
+var imgs = [];
 for(var i = 0; i < names.length; i++){
-  //makeRequest(names[i], i);
+  makeRequest(names[i], i);
 }
 
+setTimeout(function(){
+    console.log(imgs);
+},2000);
 function makeRequest(name, i){
   $.getJSON('https://api.twitch.tv/kraken/streams/' + name + '?callback=?', function(data) {
     var status = "";
@@ -14,11 +18,13 @@ function makeRequest(name, i){
       status = "removed";
     }else if(data.stream != null){
       status = "online";
+      imgs.push(data.stream.channel.logo);
     }else{
       status = "offline";
-    }
 
-    $("#status lu").append("<li id='item" + i + "'>" + name + ": " + status + "</li>");
+    }
+    var itemHTML = "<d";
+    //$("#status lu").append("<li id='item" + i + "'>" + name + ": " + status + "</li>");
   });
 }
 
@@ -27,6 +33,14 @@ function makeRequest(name, i){
 //$.getJSON('https://api.twitch.tv/kraken/search/channels?q=' + encodeURIComponent(search) + '&limit=5&callback=?', function(data){
 //  console.log(data);
 //});
+
+function getLogoOffline(channel){
+  return $.getJSON('https://api.twitch.tv/kraken/channels/' + channel, function(data){
+  });
+}
+
+var d = getLogoOffline("freecodecamp");
+console.log(d);
 
 function resizing(){
   $("#iconBar input").width($("#iconBar").width()-$("#iconBar .btn-group").width()-30);
